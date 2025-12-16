0f4dbd61b169b565720561d5e955aed586a8faca
Amith P <amithanilp@gmail.com>
2025-12-16 22:33:38 +0530

    tailwind installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './Home'
 import OfficialLogin from './pages/official/OfficialLogin'
 import Official from './pages/official/Official'
 import Login from './pages/register/Login'
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom"
-
+import './style.css'
 const Home = () => {
   const navigate = useNavigate()
 
   return (
-    <>Home
+    <><p className='text-3xl font-bold underline'>home</p>
       <br />
       <button onClick={() => navigate('/official/login')}>official</button><br />
       <button onClick={() => navigate('/agent/login')}>register</button><br />
