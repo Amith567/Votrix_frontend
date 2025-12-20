86f272611d8af542a7742aa6e3ee578da770824f
Amith P <amithanilp@gmail.com>
2025-12-20 18:44:35 +0530

    user login completed

--- client/src/pages/vote/Voting.jsx
+++ client/src/pages/vote/Voting.jsx
@@ -3,11 +3,9 @@ import { useNavigate } from 'react-router-dom'
 const Voting = () => {
     const navigate = useNavigate()
     return (
-        <div>Voting
-
-            <p>choose candidate</p>
+        <>
             <button onClick={() => navigate('/voting/success')}>vote</button>
-        </div>
+        </>
     )
 }
 
